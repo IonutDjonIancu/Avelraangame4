bf4d401f7bf2d1f5c9c37701436c27ec7b2a01c6
iancu ionut <iiancu85@gmail.com>
2026-04-28 02:17:14 +0200

    Added some changes to appsettings.

--- Avelraangame4/Program.cs
+++ Avelraangame4/Program.cs
@@ -25,7 +25,6 @@ using (var scope = app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
